7670f6688453d249da793bc729e5cb414d77d8b9
lokesh <lokesh.boddula@delhivery.com>
2024-12-21 01:26:32 +0530

    add update method implementation

--- operations.py
+++ operations.py
@@ -1,16 +1,23 @@
 import datetime
+import os
 import re
 import json
 
 curr_datetime = datetime.datetime.now()
 file_path = 'data.json'
-pattern = r"(['\"])(.*?)\1"
+description_pattern = r"(['\"])(.*?)\1"
+update_format_pattern = r"^update (\d+) \"(.*?)\"$"
 
 def add(task):
 
     data = {}
 
-    with open('data.json', 'r') as file:
+    if not os.path.exists(file_path):
+        # Create the file with some default data (e.g., an empty dictionary)
+        with open(file_path, 'w') as file:
+            json.dump([], file)
+
+    with open(file_path, 'r') as file:
         file_data = json.load(file)
 
     data['id'] = len(file_data) + 1
@@ -20,6 +27,37 @@ def add(task):
     data['updatedAt'] = str(curr_datetime)
     
     file_data.append(data)
+
+    with open(file_path, 'w') as file:
+        json.dump(file_data, file, indent=4)
+
+
+def update(task):
+    if not re.match(update_format_pattern, task):
+        print("format error!, expected format: update <id> <description>")
+        return
+
+    parts = task.split(' ', 2)
+
+    if len(parts) != 3:
+        return "Invalid input format"
+
+    task_id = int(parts[1])
+    description = parts[2].strip('"')
+
+    with open(file_path, 'r') as file:
+        file_data = json.load(file)
+
+    flag = False
+    for dict_at_index in file_data:
+        if dict_at_index['id'] == task_id:
+            dict_at_index['description'] = description
+            dict_at_index['updatedAt'] = str(curr_datetime)
+            flag = True
+            break
+
+    if not flag:
+        print("No data found for given Id :", task_id)
 
     with open(file_path, 'w') as file:
         json.dump(file_data, file, indent=4)
--- task-manager.py
+++ task-manager.py
@@ -1,9 +1,26 @@
 import operations
 
-# open a json file..
 task = input()
+
+commands = '''choose command from below 
+    add <text>
+    update <text>
+    delete <task id>
+    list <type> - leave empty for all types'''
 
 if 'add' in task:
     operations.add(task)
+elif 'update' in task:
+    operations.update(task)
+elif 'delete' in task:
+    operations.delete(task)
+elif 'list done' in task:
+    operations.list_done(task)
+elif 'list todo' in task:
+    operations.list_todo(task)
+elif 'list in-progress' in task:
+    operations.list_in_progress(task)
+elif 'list' in task:
+    operations.list_all(task)
 else:
-    print("nigga!")
+    print(commands)
