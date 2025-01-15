96ca71e12e52c884b1ef114df05eb6140f24a527
lokesh <lokesh.boddula@delhivery.com>
2025-01-15 23:31:36 +0530

    Beta 1.0

--- operations.py
+++ operations.py
@@ -3,10 +3,12 @@ import os
 import re
 import json
 
+from pyparsing import empty
+
 curr_datetime = datetime.datetime.now()
 file_path = 'data.json'
 
-def add(description):
+def add():
 
     data = {}
 
@@ -18,6 +20,8 @@ def add(description):
     with open(file_path, 'r') as file:
         file_data = json.load(file)
 
+    description = input("Enter task description: ").strip()
+
     data['id'] = len(file_data) + 1
     data['description'] = description
     data['status'] = 'todo'
@@ -25,33 +29,43 @@ def add(description):
     data['updatedAt'] = str(curr_datetime)
     
     file_data.append(data)
-
+    print("Task added!")
     with open(file_path, 'w') as file:
         json.dump(file_data, file, indent=4)
 
 
-def update(task_id, status, description):
+def update():
+    task_id = int(input("Enter task ID to update: "))
 
     with open(file_path, 'r') as file:
         file_data = json.load(file)
 
     flag = False
+    temp = {}
     for dict_at_index in file_data:
         if dict_at_index['id'] == task_id:
+            description = input("Enter the new description: ")
+            status = input("Enter new status (todo, in-progress, done): ").strip().lower()
+
             dict_at_index['description'] = description
+            temp['description'] = dict_at_index['description']
             dict_at_index['status'] = status
+            temp['status'] = dict_at_index['status']
             dict_at_index['updatedAt'] = str(curr_datetime)
             flag = True
             break
 
     if not flag:
         print("No data found for given Id :", task_id)
-
+    else:
+        print("updated task : ", temp)
     with open(file_path, 'w') as file:
         json.dump(file_data, file, indent=4)
 
 
-def list_tasks(status=None):
+def list_tasks():
+
+    status = input("Enter status to filter by (all, todo, in-progress, done): ").strip().lower()
 
     with open(file_path, 'r') as file:
         file_data = json.load(file)
@@ -59,12 +73,21 @@ def list_tasks(status=None):
     tasks = file_data
     if status:
         tasks = [t for t in tasks if t['status'] == status]
+    else:
+        tasks = [t for t in tasks]
+
+    if not tasks:
+        print("[]")
+        return
+
     for task in tasks:
-        print(
-            f"ID: {task['id']}, Status: {task['status']}, Description: {task['description']}")
+        print(f"ID: {task['id']}, Status: {task['status']}, Description: {task['description']}")
 
 
-def delete(task_id):
+
+def delete():
+
+    task_id = int(input("Enter task ID to delete: "))
 
     with open(file_path, 'r') as file:
         file_data = json.load(file)
--- task-manager.py
+++ task-manager.py
@@ -4,25 +4,16 @@ while True:
     command = input("Enter command (add, update, delete, list, exit): ").strip().lower()
 
     if command == "add":
-        description = input("Enter task description: ").strip()
-        operations.add(description)
+        operations.add()
 
     elif command == "update":
-        task_id = int(input("Enter task ID to update: "))
-        description = input("Enter the new description: ")
-        status = input("Enter new status (todo, in-progress, done): ").strip().lower()
-        operations.update(task_id, status, description)
+        operations.update()
 
     elif command == "delete":
-        task_id = int(input("Enter task ID to delete: "))
-        operations.delete(task_id)
+        operations.delete()
 
     elif command == "list":
-        filter_status = input("Enter status to filter by (all, todo, in-progress, done): ").strip().lower()
-        if filter_status == 'all':
-            operations.list_tasks()
-        else:
-            operations.list_tasks(filter_status)
+        operations.list_tasks()
 
     elif command == "exit":
         print("Exiting the Task Tracker.")
